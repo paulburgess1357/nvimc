90f06aa55d96099c781c2803631b3d996607c610
Paul B <paulburgess1357@gmail.com>
2025-12-11 21:33:22 -0700

    update

--- lsp_test/cpp/build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-mkdir -p build
-cd build
-cmake .. -DCMAKE_EXPORT_COMPILE_COMMANDS=ON
-make
-
-# Copy compile_commands.json to project root for clangd
-cp compile_commands.json ..
--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -15,6 +15,41 @@ return {
       "williamboman/mason-lspconfig.nvim",
     },
     config = function()
+      -- LSP keybindings (set when LSP attaches to buffer)
+      vim.api.nvim_create_autocmd("LspAttach", {
+        group = vim.api.nvim_create_augroup("lsp-attach", { clear = true }),
+        callback = function(event)
+          local map = function(keys, func, desc)
+            vim.keymap.set("n", keys, func, { buffer = event.buf, desc = "LSP: " .. desc })
+          end
+
+          -- Navigation
+          map("gd", vim.lsp.buf.definition, "Go to definition")
+          map("gD", vim.lsp.buf.declaration, "Go to declaration")
+          map("gr", vim.lsp.buf.references, "Go to references")
+          map("gI", vim.lsp.buf.implementation, "Go to implementation")
+          map("gy", vim.lsp.buf.type_definition, "Go to type definition")
+
+          -- Documentation
+          map("K", vim.lsp.buf.hover, "Hover documentation")
+          map("gK", vim.lsp.buf.signature_help, "Signature help")
+
+          -- Code actions
+          map("<leader>ca", vim.lsp.buf.code_action, "Code action")
+          map("<leader>cr", vim.lsp.buf.rename, "Rename symbol")
+
+          -- Diagnostics
+          map("<leader>cd", vim.diagnostic.open_float, "Line diagnostics")
+          map("[d", vim.diagnostic.goto_prev, "Previous diagnostic")
+          map("]d", vim.diagnostic.goto_next, "Next diagnostic")
+
+          -- Format
+          map("<leader>cf", function()
+            vim.lsp.buf.format({ async = true })
+          end, "Format buffer")
+        end,
+      })
+
       -- Custom clangd configuration using vim.lsp.config (Neovim 0.11+)
       vim.lsp.config.clangd = {
         cmd = {
