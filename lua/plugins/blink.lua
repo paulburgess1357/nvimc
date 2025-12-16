e12011b70d48ed288e83f5d0549f2a6916aa1fcb
Paul B <paulburgess1357@gmail.com>
2025-12-15 22:07:44 -0700

    blink

---
+++ lua/plugins/blink.lua
@@ -0,0 +1,47 @@
+return {
+  "saghen/blink.cmp",
+  version = "*",
+  opts = {
+    keymap = {
+      preset = "default",
+      ["<Tab>"] = {
+        function(cmp)
+          if cmp.snippet_active() then
+            return cmp.accept()
+          else
+            return cmp.select_and_accept()
+          end
+        end,
+        "snippet_forward",
+        "fallback",
+      },
+      ["<S-Tab>"] = { "snippet_backward", "fallback" },
+      ["<C-j>"] = { "select_next", "fallback" },
+      ["<C-k>"] = { "select_prev", "fallback" },
+    },
+    appearance = {
+      nerd_font_variant = "mono",
+    },
+    sources = {
+      default = { "lsp", "path", "snippets", "buffer" },
+    },
+    completion = {
+      menu = {
+        border = "rounded",
+      },
+      documentation = {
+        auto_show = true,
+        auto_show_delay_ms = 200,
+        window = {
+          border = "rounded",
+        },
+      },
+    },
+    signature = {
+      enabled = true,
+      window = {
+        border = "rounded",
+      },
+    },
+  },
+}
