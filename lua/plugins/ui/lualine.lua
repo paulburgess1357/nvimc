29d2b5d2c5b1c89e55361d39d54d51233d26c230
Paul B <paulburgess1357@gmail.com>
2025-12-17 08:20:47 -0700

    lualine

--- lua/plugins/ui/lualine.lua
+++ lua/plugins/ui/lualine.lua
@@ -8,7 +8,7 @@ return {
   },
   opts = {
     options = {
-      theme = "onedark",
+      theme = "auto",
       icons_enabled = true,
       component_separators = { left = "|", right = "|" },
       section_separators = { left = "", right = "" },
