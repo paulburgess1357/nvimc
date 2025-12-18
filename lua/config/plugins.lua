6b3d3919b3a5e29600f6e571ff93e8cddcb21ef1
Paul B <paulburgess1357@gmail.com>
2025-12-18 08:29:30 -0700

    lnc

--- lua/config/plugins.lua
+++ lua/config/plugins.lua
@@ -6,35 +6,38 @@
 -- If branch is omitted, uses plugin default
 
 return {
-  -- Coding
-  treesitter = { enabled = true, branch = "main" },
-  lsp = { enabled = true },
-  mason = { enabled = true },
-  blink = { enabled = true },
-  conform = { enabled = true },
-  lint = { enabled = true },
-  increname = { enabled = true },
-  pairs = { enabled = true },
+	-- Coding
+	treesitter = { enabled = true, branch = "main" },
+	lsp = { enabled = true },
+	mason = { enabled = true },
+	blink = { enabled = true },
+	conform = { enabled = true },
+	lint = { enabled = true },
+	increname = { enabled = true },
+	pairs = { enabled = true },
 
-  -- Editor
-  minifiles = { enabled = true },
-  fzf = { enabled = true },
-  gitsigns = { enabled = true },
-  spider = { enabled = true },
-  illuminate = { enabled = true },
-  todocomments = { enabled = true },
-  marks = { enabled = true },
-  rendermarkdown = { enabled = true },
+	-- Editor
+	minifiles = { enabled = true },
+	fzf = { enabled = true },
+	gitsigns = { enabled = true },
+	spider = { enabled = true },
+	illuminate = { enabled = true },
+	todocomments = { enabled = true },
+	marks = { enabled = true },
+	rendermarkdown = { enabled = true },
 
-  -- UI
-  colorscheme = { enabled = true },
-  lualine = { enabled = true },
-  bufferline = { enabled = true },
-  whichkey = { enabled = true },
-  snacks = { enabled = true },
-  noice = { enabled = true },
-  incline = { enabled = true },
-  navic = { enabled = true },
-  rainbowdelimiters = { enabled = true },
-  scrollbar = { enabled = true },
+	-- UI
+	colorscheme = { enabled = true },
+	lualine = { enabled = true },
+	bufferline = { enabled = true },
+	whichkey = { enabled = true },
+	snacks = { enabled = true },
+	noice = { enabled = true },
+	incline = { enabled = true },
+	navic = { enabled = true },
+	rainbowdelimiters = { enabled = true },
+	scrollbar = { enabled = true },
+
+	-- Custom
+	leetneo = { enabled = true, branch = "master" },
 }
---
+++ lua/plugins/coding/leetneo.lua
@@ -0,0 +1,18 @@
+-- LeetNeoCode: LeetCode problem solving in Neovim.
+-- Fetch, solve, and manage LeetCode problems with multi-language support.
+local cfg = require("config.plugins").leetneo or {}
+return {
+	"paulburgess1357/LeetNeoCode",
+	enabled = cfg.enabled ~= false,
+	branch = cfg.branch,
+	dir = "/home/paul/Repos/LeetNeoCode",
+	cmd = { "LC", "LCPull", "LCCopy", "LCRecent", "LCKeywords", "LCFold" },
+	opts = {
+		cache_dir = "/home/paul/.cache/nvim/LeetNeoCode",
+		default_language = "cpp",
+		code_only = true,
+		enable_images = false,
+		smart_copy = true,
+		cache_expiry_days = 14,
+	},
+}
