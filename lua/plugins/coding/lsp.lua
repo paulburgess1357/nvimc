e1f67ef340ba66964991c4c9e651f57091eb0bf0
Paul B <paulburgess1357@gmail.com>
2025-12-17 19:23:33 -0700

    update

--- lua/plugins/coding/lsp.lua
+++ lua/plugins/coding/lsp.lua
@@ -1,126 +1,144 @@
 -- LSP: Language Server Protocol support for code intelligence.
 -- Provides auto-completion, go-to-definition, diagnostics, and code actions.
 return {
-  -- SchemaStore: JSON/YAML schema catalog for validation
-  { "b0o/SchemaStore.nvim", lazy = true },
+	-- SchemaStore: JSON/YAML schema catalog for validation
+	{ "b0o/SchemaStore.nvim", lazy = true },
 
-  -- Mason: Package manager for LSP servers, linters, and formatters
-  {
-    "mason-org/mason.nvim",
-    opts = {},
-  },
+	-- Mason: Package manager for LSP servers, linters, and formatters
+	{
+		"mason-org/mason.nvim",
+		opts = {},
+	},
 
-  -- LSP configurations and keybindings
-  {
-    "neovim/nvim-lspconfig",
-    dependencies = {
-      "mason-org/mason.nvim",
-      "mason-org/mason-lspconfig.nvim",
-    },
-    config = function()
-      vim.api.nvim_create_autocmd("LspAttach", {
-        group = vim.api.nvim_create_augroup("lsp-attach", { clear = true }),
-        callback = function(event)
-          local map = function(keys, func, desc)
-            vim.keymap.set("n", keys, func, { buffer = event.buf, desc = "LSP: " .. desc })
-          end
+	-- LSP configurations and keybindings
+	{
+		"neovim/nvim-lspconfig",
+		dependencies = {
+			"mason-org/mason.nvim",
+			"mason-org/mason-lspconfig.nvim",
+		},
+		config = function()
+			vim.api.nvim_create_autocmd("LspAttach", {
+				group = vim.api.nvim_create_augroup("lsp-attach", { clear = true }),
+				callback = function(event)
+					local map = function(keys, func, desc)
+						vim.keymap.set("n", keys, func, { buffer = event.buf, desc = "LSP: " .. desc })
+					end
 
-          -- Navigation (using fzf-lua)
-          map("gd", function() require("fzf-lua").lsp_definitions() end, "Go to definition")
-          map("gD", vim.lsp.buf.declaration, "Go to declaration")
-          map("gr", function() require("fzf-lua").lsp_references() end, "Go to references")
-          map("gI", function() require("fzf-lua").lsp_implementations() end, "Go to implementation")
-          map("gy", function() require("fzf-lua").lsp_typedefs() end, "Go to type definition")
+					-- Navigation (using fzf-lua)
+					map("gd", function()
+						require("fzf-lua").lsp_definitions()
+					end, "Go to definition")
+					map("gD", vim.lsp.buf.declaration, "Go to declaration")
+					map("gr", function()
+						require("fzf-lua").lsp_references()
+					end, "Go to references")
+					map("gI", function()
+						require("fzf-lua").lsp_implementations()
+					end, "Go to implementation")
+					map("gy", function()
+						require("fzf-lua").lsp_typedefs()
+					end, "Go to type definition")
 
-          -- Documentation
-          map("K", vim.lsp.buf.hover, "Hover documentation")
-          map("gK", vim.lsp.buf.signature_help, "Signature help")
+					-- Documentation
+					map("K", vim.lsp.buf.hover, "Hover documentation")
+					map("gK", vim.lsp.buf.signature_help, "Signature help")
 
-          -- Code actions (using fzf-lua)
-          map("<leader>ca", function() require("fzf-lua").lsp_code_actions() end, "Code action")
-          -- Rename is handled by inc-rename.nvim
+					-- Code actions (using fzf-lua)
+					map("<leader>ca", function()
+						require("fzf-lua").lsp_code_actions()
+					end, "Code action")
+					-- Rename is handled by inc-rename.nvim
 
-          -- Diagnostics (using fzf-lua)
-          map("<leader>cd", function() require("fzf-lua").diagnostics_document() end, "Document diagnostics")
-          map("[d", function() vim.diagnostic.jump({ count = -1, float = true }) end, "Previous diagnostic")
-          map("]d", function() vim.diagnostic.jump({ count = 1, float = true }) end, "Next diagnostic")
+					-- Diagnostics (using fzf-lua)
+					map("<leader>cd", function()
+						require("fzf-lua").diagnostics_document()
+					end, "Document diagnostics")
+					map("[d", function()
+						vim.diagnostic.jump({ count = -1, float = true })
+					end, "Previous diagnostic")
+					map("]d", function()
+						vim.diagnostic.jump({ count = 1, float = true })
+					end, "Next diagnostic")
 
-          -- Format
-          map("<leader>cf", function()
-            vim.lsp.buf.format({ async = true })
-          end, "Format buffer")
-        end,
-      })
+					-- Format
+					map("<leader>cf", function()
+						vim.lsp.buf.format({ async = true })
+					end, "Format buffer")
+				end,
+			})
 
-      -- Custom clangd configuration using vim.lsp.config (Neovim 0.11+)
-      vim.lsp.config.clangd = {
-        cmd = {
-          "clangd",
-          "--background-index",
-          "--clang-tidy",
-          "--header-insertion=iwyu",
-          "--completion-style=detailed",
-          "--function-arg-placeholders=true",
-          "--enable-config",
-        },
-        filetypes = { "c", "cpp", "objc", "objcpp", "cuda", "proto" },
-        root_markers = {
-          ".clangd",
-          ".clang-tidy",
-          ".clang-format",
-          "compile_commands.json",
-          "compile_flags.txt",
-          ".git",
-        },
-      }
+			-- Custom clangd configuration using vim.lsp.config (Neovim 0.11+)
+			vim.lsp.config.clangd = {
+				cmd = {
+					"clangd",
+					"--background-index",
+					"--clang-tidy",
+					"--header-insertion=never",
+					"--completion-style=detailed",
+					"--function-arg-placeholders=true",
+					"--enable-config",
+					"--limit-references=0",
+					"--limit-results=0",
+				},
+				filetypes = { "c", "cpp", "objc", "objcpp", "cuda", "proto" },
+				root_markers = {
+					".clangd",
+					".clang-tidy",
+					".clang-format",
+					"compile_commands.json",
+					"compile_flags.txt",
+					".git",
+				},
+			}
 
-      -- JSON LSP with SchemaStore
-      vim.lsp.config.jsonls = {
-        settings = {
-          json = {
-            schemas = require("schemastore").json.schemas(),
-            validate = { enable = true },
-          },
-        },
-      }
+			-- JSON LSP with SchemaStore
+			vim.lsp.config.jsonls = {
+				settings = {
+					json = {
+						schemas = require("schemastore").json.schemas(),
+						validate = { enable = true },
+					},
+				},
+			}
 
-      -- YAML LSP with SchemaStore
-      vim.lsp.config.yamlls = {
-        settings = {
-          yaml = {
-            schemaStore = { enable = false, url = "" },
-            schemas = require("schemastore").yaml.schemas(),
-            validate = true,
-          },
-        },
-      }
-    end,
-  },
+			-- YAML LSP with SchemaStore
+			vim.lsp.config.yamlls = {
+				settings = {
+					yaml = {
+						schemaStore = { enable = false, url = "" },
+						schemas = require("schemastore").yaml.schemas(),
+						validate = true,
+					},
+				},
+			}
+		end,
+	},
 
-  -- Mason-LSPconfig: Bridge between Mason and lspconfig
-  {
-    "mason-org/mason-lspconfig.nvim",
-    dependencies = {
-      "mason-org/mason.nvim",
-      "neovim/nvim-lspconfig",
-    },
-    opts = {
-      ensure_installed = {
-        -- Languages
-        "lua_ls",
-        "clangd",
-        "pyright",
-        "ruby_lsp",
-        "bashls",
-        -- Config/Data
-        "jsonls",
-        "yamlls",
-        "taplo",
-        -- Containers
-        "dockerls",
-        "docker_compose_language_service",
-      },
-      automatic_enable = true,
-    },
-  },
+	-- Mason-LSPconfig: Bridge between Mason and lspconfig
+	{
+		"mason-org/mason-lspconfig.nvim",
+		dependencies = {
+			"mason-org/mason.nvim",
+			"neovim/nvim-lspconfig",
+		},
+		opts = {
+			ensure_installed = {
+				-- Languages
+				"lua_ls",
+				"clangd",
+				"pyright",
+				"ruby_lsp",
+				"bashls",
+				-- Config/Data
+				"jsonls",
+				"yamlls",
+				"taplo",
+				-- Containers
+				"dockerls",
+				"docker_compose_language_service",
+			},
+			automatic_enable = true,
+		},
+	},
 }
