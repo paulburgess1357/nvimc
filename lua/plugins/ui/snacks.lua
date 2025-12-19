cedfbdf99c4ff4d38424de6788d1e0fb0c8e1a50
Paul B <paulburgess1357@gmail.com>
2025-12-18 18:45:41 -0700

    Remove icon

--- lua/plugins/ui/snacks.lua
+++ lua/plugins/ui/snacks.lua
@@ -58,7 +58,7 @@ return {
           { icon = " ", key = "g", desc = "Find Text", action = ":lua Snacks.dashboard.pick('live_grep')" },
           { icon = " ", key = "r", desc = "Recent Files", action = ":lua Snacks.dashboard.pick('oldfiles')" },
           { icon = " ", key = "c", desc = "Config", action = ":lua Snacks.dashboard.pick('files', {cwd = vim.fn.stdpath('config')})" },
-          { icon = "󰒲 ", key = "l", desc = "Lazy", action = ":Lazy" },
+          { icon = " ", key = "l", desc = "Lazy", action = ":Lazy" },
           { icon = " ", key = "q", desc = "Quit", action = ":qa" },
         },
       },
