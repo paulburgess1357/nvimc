274900d88492b3bc5c2cbee0cecaf602d30bc257
Paul B <paulburgess1357@gmail.com>
2025-12-18 18:17:50 -0700

    update

--- lua/plugins/ui/incline.lua
+++ lua/plugins/ui/incline.lua
@@ -29,12 +29,13 @@ return {
 
       local ft_icon, ft_color = require("nvim-web-devicons").get_icon_color(filename)
       local modified = vim.bo[props.buf].modified
+      local modified_color = vim.api.nvim_get_hl(0, { name = "DiagnosticWarn" }).fg
 
       return {
         { " " },
         ft_icon and { ft_icon, " ", guifg = ft_color } or "",
         { filename, gui = modified and "bold,italic" or "bold" },
-        modified and { " ", "", guifg = "#e0af68" } or "",
+        modified and { " ", "", guifg = modified_color } or "",
         { " " },
       }
     end,
