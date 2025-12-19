1d8e9f0e39ee54d8e0b6d321d772da61fb6f8a91
Paul B <paulburgess1357@gmail.com>
2025-12-19 08:33:45 -0700

    format

--- init.lua
+++ init.lua
@@ -1,14 +1,14 @@
 -- Bootstrap lazy.nvim plugin manager
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.uv.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable",
-    lazypath,
-  })
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable",
+		lazypath,
+	})
 end
 vim.opt.rtp:prepend(lazypath)
 
@@ -18,11 +18,11 @@ require("config.keymaps")
 
 -- Load plugins
 require("lazy").setup({
-  { import = "plugins.coding" },
-  { import = "plugins.editor" },
-  { import = "plugins.ui" },
+	{ import = "plugins.coding" },
+	{ import = "plugins.editor" },
+	{ import = "plugins.ui" },
 }, {
-  change_detection = {
-    notify = false,
-  },
+	change_detection = {
+		notify = false,
+	},
 })
