06b4b252545f327018f31213113f5d56bdb1c78b
Paul B <paulburgess1357@gmail.com>
2026-01-29 08:20:00 -0700

    Auto-disable personal plugins before push

--- lua/config/plugins.lua
+++ lua/config/plugins.lua
@@ -58,5 +58,5 @@ return {
 	mcphub = { enabled = true },
 
 	-- Personal (auto-disabled on push via .git/hooks/pre-push)
-	leetneo = { enabled = true, branch = "master" },
+	leetneo = { enabled = false, branch = "master" },
 }
