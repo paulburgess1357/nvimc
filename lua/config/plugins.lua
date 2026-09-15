af17bd8cd5e366dd33404d2bbdb0d029fc9680f3
Paul B <paulburgess1357@gmail.com>
2026-09-15 08:33:16 -0600

    Auto-disable personal plugins before push

--- lua/config/plugins.lua
+++ lua/config/plugins.lua
@@ -51,5 +51,5 @@ return {
 	dap = { enabled = true }, -- Debug adapter protocol support
 
 	-- Personal (auto-disabled on push via .git/hooks/pre-push)
-	leetneo = { enabled = true }, -- LeetCode integration
+	leetneo = { enabled = false }, -- LeetCode integration
 }
